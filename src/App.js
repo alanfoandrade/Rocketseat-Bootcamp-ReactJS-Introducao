53e650d1d01cff775687d71f265e4e89f32b911a
alanfoandrade <alanfrank@gec.inatel.br>
2019-10-31 14:27:14 -0300

    Add component TechList

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import React from 'react';
+import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
-  );
+import TechList from './components/TechList';
+
+export default function App() {
+  return <TechList />;
 }
-
-export default App;
---
+++ src/components/TechList.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+export default class Techlist extends Component {
+  state = {
+    techs: ['Node.js', 'ReactJS', 'React Native'],
+  };
+
+  render() {
+    console.log(this.state);
+    return (
+      <ul>
+        <li>Node.js</li>
+        <li>ReactJS</li>
+        <li>React Native</li>
+      </ul>
+    );
+  }
+}
