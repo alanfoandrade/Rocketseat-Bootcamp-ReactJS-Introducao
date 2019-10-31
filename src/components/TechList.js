d187202f1d0cf1f07e9f5ca9b0973a238b5e8f48
alanfoandrade <alanfrank@gec.inatel.br>
2019-10-31 15:19:28 -0300

    Add e Remove itens do estado

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -18,13 +18,23 @@ export default class Techlist extends Component {
     this.setState({ techs: [...techs, newTech], newTech: '' });
   };
 
+  handleDelete = tech => {
+    const { techs } = this.state;
+    this.setState({ techs: techs.filter(t => t !== tech) });
+  };
+
   render() {
     const { techs, newTech } = this.state;
     return (
       <form onSubmit={this.handleSubmit}>
         <ul>
           {techs.map(tech => (
-            <li key={tech}>{tech}</li>
+            <li key={tech}>
+              {tech}
+              <button onClick={() => this.handleDelete(tech)} type="button">
+                Remover
+              </button>
+            </li>
           ))}
         </ul>
         <input
